adf272ae1f17a55d4f419e3eba0be60f20494287
songyunzu <2365547115@qq.com>
2020-04-14 11:55:58 +0800

    完成parser.items和其测试

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from myparser.cfg import Cfg
 from myparser.item import Item
+from myparser.parser import Parser
 
 if __name__ == '__main__':
     def test_cfg():
@@ -28,6 +29,16 @@ if __name__ == '__main__':
         test_next_item()
         print("next_symbol")
         test_next_symbol()
+
+    def test_parser_items():
+        parser = Parser()
+        parser.items(parser.cfg)
+        k = 0
+        for items in parser.item_family:
+            print("S" + str(k))
+            for i in items:
+                print(i)
+
 
 
     test_cfg()
